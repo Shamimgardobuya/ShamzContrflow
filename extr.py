f9c6322306c512c9c6491caf9a5c14b1952e35c8
Shamimgardobuya <shamimobuya@gmail.com>
2022-04-24 11:47:21 +0300

    did control flow exercise

---
+++ extr.py
@@ -0,0 +1,55 @@
+python3
+Python 3.8.10 (default, Mar 15 2022, 12:22:08)
+[GCC 9.4.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 10/3 - 10%3
+2.3333333333333335
+>>> 10**3
+1000
+>>> x=1
+>>> (x+=2)
+  File "<stdin>", line 1
+    (x+=2)
+      ^
+SyntaxError: invalid syntax
+>>> x+=2
+>>> x
+3
+>>> #we use rounding off,use round()
+>>> round(7.8)
+8
+>>> #result for floata(1)
+>>> float(1)
+1.0
+>>> #bool(“False”)?
+>>> bool("False")
+True
+>>> #values that evaluate to False in a boolean context.
+>>> 10=='10'
+False
+>>> 'bag'>" not(True or False)?apple'
+  File "<stdin>", line 1
+    'bag'>" not(True or False)?apple'
+                                    ^
+SyntaxError: EOL while scanning string literal
+>>> 'bag'>'apple'
+True
+>>>  18 <= age < 65
+  File "<stdin>", line 1
+    18 <= age < 65
+    ^
+IndentationError: unexpected indent
+>>> #what does ange tr
+KeyboardInterrupt
+>>> range(1,10,2)
+range(1, 10, 2)  
+return range(1,10,2)
+  File "<stdin>", line 1
+SyntaxError: 'return' outside function
+
+>>> #Iterable objects
+>>> Lists,Dictionary,tuples,set
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'Lists' is not defined
+>>>List,Dictionary,tuples,set
